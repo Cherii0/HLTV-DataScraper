743a544469a79271b3218e52d8d67260d2e048a4
Cherii0 <zgmnt.main@gmail.com>
2025-03-06 16:15:35 +0100

    scrapping teams and scores from front page of results

--- get_results_page.py
+++ get_results_page.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import re
 
 driver = webdriver.Chrome(options=Options())
 url = "https://www.hltv.org/results"
@@ -11,11 +12,25 @@ driver.implicitly_wait(5)
 page_content = driver.page_source
 
 soup = BeautifulSoup(page_content, "html.parser")
+soup.prettify()
+matches = soup.find_all("div", class_="result-con")
 
-table = soup.find_all("div", class_="main-content")
-
-# encoding="utf-8" required for properly write file 
 with open("hltv.txt", "w", encoding="utf-8") as file:
-    file.write(str(soup))
+    for match_no, match in enumerate(matches):
+        match_link = match.find("a", class_="a-reset").get("href")
+        score_won = match.find("span", class_="score-won").get_text()
+        score_lost = match.find("span", class_="score-lost").get_text()
+        for i in range(1,3):
+            team_content =  match.find("div", class_=f'line-align team{i}')
+            img_content = team_content.find("img")
+            team_name = img_content.get("title")
+            if i == 1:
+                team1 = team_name
+            else:
+                team2 = team_name
+        res = f"match no : {match_no} {team1}  vs  {team2}  scores :  {score_won} - {score_lost} \n "
+        
+        # encoding="utf-8" required for properly write file
+        file.write(str(res))
 
 driver.quit()
