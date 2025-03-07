d6f85614301adb71395205295cfedb2fe5ee8141
Cherii0 <zgmnt.main@gmail.com>
2025-03-07 15:07:58 +0100

    split code into functions, added get_matches_links()

--- get_results_page.py
+++ get_results_page.py
@@ -1,3 +1,5 @@
+from operator import concat
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
@@ -8,16 +10,25 @@ url = "https://www.hltv.org/results"
 driver.get(url)
 
 # waits for page to fully load
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 page_content = driver.page_source
 
 soup = BeautifulSoup(page_content, "html.parser")
 soup.prettify()
-matches = soup.find_all("div", class_="result-con")
 
-with open("hltv.txt", "w", encoding="utf-8") as file:
-    for match_no, match in enumerate(matches):
-        match_link = match.find("a", class_="a-reset").get("href")
+# holds content to scrap
+matches_content = soup.find_all("div", class_="result-con")
+
+def get_matches_links(matches_content):
+    matches_links = []
+    for match in matches_content:
+        full_link = concat("https://www.hltv.org", match.find("a", class_="a-reset").get("href"))
+        matches_links.append(full_link)
+    return matches_links
+
+def get_match_overview(matches_content):
+    matches_overview = []
+    for match_no, match in enumerate(matches_content):
         score_won = match.find("span", class_="score-won").get_text()
         score_lost = match.find("span", class_="score-lost").get_text()
         for i in range(1,3):
@@ -28,9 +39,24 @@ with open("hltv.txt", "w", encoding="utf-8") as file:
                 team1 = team_name
             else:
                 team2 = team_name
-        res = f"match no : {match_no} {team1}  vs  {team2}  scores :  {score_won} - {score_lost} \n "
-        
-        # encoding="utf-8" required for properly write file
-        file.write(str(res))
+        res = {
+                "match_no" : match_no,
+                "team1" : team1,
+                "team2" : team2,
+                "score_won" : score_won,
+                "score_lost" : score_lost
+              }
+        matches_overview.append(res)
+    return(matches_overview)
+
+
+def write_to_file(match_overview, file_name):
+    # encoding="utf-8" required for properly write file
+    with open(file_name, "w", encoding="utf-8") as file:
+        for match in match_overview:
+            file.write(concat(str(match), "\n"))
+
+
+write_to_file(get_match_overview(matches_content), "hltv.txt")
 
 driver.quit()
